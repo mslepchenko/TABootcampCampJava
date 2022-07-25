5c8874ab3cb71000efbea37765be98b68c776d48
mslepchenko <psihea.margo@gmail.com>
2022-07-25 17:06:36 +0300

    FindLargest

---
+++ TABootcamp/src/com/bootcampexcercise/module2/activity/FindLargest.java
@@ -0,0 +1,12 @@
+package com.bootcampexcercise.module2.activity;
+
+public class FindLargest {
+    public static void main(String[] args) {
+        int i = 7;
+        int j = 10;
+        if (i>j) {
+            System.out.println("i is larger that j");
+        } else {
+                System.out.println ("j is larger that i");
+        }
+} }
