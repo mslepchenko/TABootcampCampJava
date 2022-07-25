cb1a43fdd4ed8486232aa0efa28198e518ae1024
mslepchenko <psihea.margo@gmail.com>
2022-07-25 16:27:52 +0300

    OperatorActivity

---
+++ TABootcamp/src/com/bootcampexcercise/module2/activity/OperatorActivity.java
@@ -0,0 +1,10 @@
+package com.bootcampexcercise.module2.activity;
+
+public class OperatorActivity {
+    public static void main(String[] args) {
+        int i = 7;
+        int j = 10;
+
+        System.out.println("The sum of a and b is = " + Integer.sum(i,j));
+    }
+}
