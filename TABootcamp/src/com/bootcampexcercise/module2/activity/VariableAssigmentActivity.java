c05c1aa5c4b3e9bef417486de0a63ec8e6efd38a
mslepchenko <psihea.margo@gmail.com>
2022-07-25 14:54:50 +0300

    VariableActicity1

---
+++ TABootcamp/src/com/bootcampexcercise/module2/activity/VariableAssigmentActivity.java
@@ -0,0 +1,9 @@
+package com.bootcampexcercise.module2.activity;
+
+public class VariableAssigmentActivity {
+    public static void main(String[] args) {
+        int i;
+        i = 7;
+        System.out.println("Variable i = " +i);
+    }
+}
