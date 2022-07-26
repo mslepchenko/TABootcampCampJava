8706b69c31bafbaa5d91d6c6d132cbcbbcebdbc6
mslepchenko <psihea.margo@gmail.com>
2022-07-26 21:08:31 +0300

    FindArray

--- TABootcamp/src/com/bootcampexcercise/module2/activity/FindInArray.java
+++ TABootcamp/src/com/bootcampexcercise/module2/activity/FindInArray.java
@@ -4,26 +4,27 @@ public class FindInArray {
 
     public static void main(String[] args) {
         // Create an integer array
-        int[] nums = {100, 1, 4, 15};
+        int[] nums = {1, 24, 56, 15};
+
         int result = nums[0];
+
         // Find smallest in an array
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] < result) {
                 result = nums[i];
             }
         }
-        System.out.println("Smallest in array is "+result);
+        System.out.println("Smallest in array is " + result);
         result = nums[0];
-        //TODO Find largest in an array
-        int[] num = {100, 1, 4, 15};
-        int result1 = num[0];
-        for (int i = 1; i > num.length; i++) {
-            if (num[i] > result1){
-                result1 = num[i];
+
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > result) {
+                result = nums[i];
             }
-            //TODO complete the code
         }
-        System.out.println("Largest in array is "+result1);
+        System.out.println("Largest in array is " + result);
+
 
     }
 }
---
+++ TABootcamp/src/com/bootcampexcercise/module2/activity/NumToWordsUsingMethod.java
@@ -0,0 +1,12 @@
+package com.bootcampexcercise.module2.activity;
+
+public class NumToWordsUsingMethod {
+    public static void main(String args[]) {
+
+        // Call method here
+    }
+
+    public static void numToWordsLogic(int x) {
+        //logic of numToWords Class
+    }
+}
