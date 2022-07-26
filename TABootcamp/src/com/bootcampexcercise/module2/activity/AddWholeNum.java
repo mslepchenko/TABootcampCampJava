5db3f86338b9240243d102ac3ca83a046b225d1c
mslepchenko <psihea.margo@gmail.com>
2022-07-26 09:46:12 +0300

    AddWholeNum

--- TABootcamp/src/com/bootcampexcercise/module2/activity/AddWholeNum.java
+++ TABootcamp/src/com/bootcampexcercise/module2/activity/AddWholeNum.java
@@ -3,9 +3,10 @@ package com.bootcampexcercise.module2.activity;
 public class AddWholeNum {
     public static void main(String[] args) {
 
-        for(int i=50;i<=100;i++) {
-            System.out.println(i);
-        }
-
+        int sum = 0;
+                for (int i = 50; i<=100; i++) {
+              sum = sum + i;
+                }
+        System.out.println("The number is " +sum);
     }
 }
