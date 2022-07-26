f088d5be50f8ba4beac1149f43456fd9753e7ba6
mslepchenko <psihea.margo@gmail.com>
2022-07-26 09:30:02 +0300

    AddWholeNum

---
+++ TABootcamp/src/com/bootcampexcercise/module2/activity/AddWholeNum.java
@@ -0,0 +1,11 @@
+package com.bootcampexcercise.module2.activity;
+
+public class AddWholeNum {
+    public static void main(String[] args) {
+
+        for(int i=50;i<=100;i++) {
+            System.out.println(i);
+        }
+
+    }
+}
